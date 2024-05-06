c89a18ed01ae42d9dff0e49a622dcf397048112c
cavo1919 <fuadq5496@gmail.com>
2024-05-06 15:54:26 +0400

    Add files via upload

---
+++ kai001/Program.cs
@@ -0,0 +1,313 @@
+﻿using System.Media;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int l = 0;
+            string bl="";
+            for (int ws = 0; ws < 14; ws++)
+            {
+            Console.WriteLine("                                                                                                                        ");
+            }
+            Console.WriteLine("                                               starting os                                                              ");
+            while (l < 119)
+            {
+                Thread.Sleep(1);
+                Console.Write($"{bl}|");
+                l++;
+            }
+            Console.Clear();
+            Console.WriteLine("runing ServicesSys.cs");
+            Thread.Sleep(100);
+            Console.WriteLine("runing service.cs");
+            Thread.Sleep(100);
+            Console.WriteLine("runing interface.cs");
+            Thread.Sleep(100);
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            for (int ws = 0; ws < 10; ws++)
+            {
+            Console.WriteLine("|                                                                                                                      |");
+            }
+            Console.WriteLine("|                                          Welcome to VIANIA OS                                                        |");
+            for (int ws = 0; ws < 16; ws++)
+            {
+            Console.WriteLine("|                                                                                                                      |");
+            }
+            Console.WriteLine("|______________________________________________________________________________________________________________________|");
+            Thread.Sleep(400);
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Green;
+            void hapsl()
+            {
+                for (int ws = 0; ws < 6; ws++)
+                {
+                    Console.WriteLine("|                                                                                                                      |");
+                }
+                Console.WriteLine("|                                                                                                                      |");
+            }
+            hapsl(); slct(0);
+            void stasl(string sme = "", string type = "")
+            {
+                if (type == "L")
+                {
+                    Console.Write("|                                       "); Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Write($"{sme}"); Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("                                       |");
+                }
+                Console.Write("|                                       "); Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write($"{sme}"); Console.BackgroundColor = ConsoleColor.Green;
+                Console.Write("                                       |");
+            }
+            void slct(int ns)
+            {
+                if (ns == 8)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("|              Calculator              |");
+                    stasl("|______________________________________|");
+                    stasl("               File Manager             ");
+                    stasl("                                        ");
+                    stasl("               Notepad L                ");
+                    stasl("                                        ");
+                    stasl("               Settings                 ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                }
+                if (ns == 7)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("               Calculator               ");
+                    stasl(" ______________________________________ ");
+                    stasl("|              File Manager            |");
+                    stasl("|______________________________________|");
+                    stasl("               Notepad L                ");
+                    stasl("                                        ");
+                    stasl("               Settings                 ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                }
+                if (ns == 6)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("               Calculator               ");
+                    stasl("                                        ");
+                    stasl("               File Manager             ");
+                    stasl(" ______________________________________ ");
+                    stasl("|              Notepad L               |");
+                    stasl("|______________________________________|");
+                    stasl("               Settings                 ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                }
+                if (ns == 5)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("               Calculator               ");
+                    stasl("                                        ");
+                    stasl("               File Manager             ");
+                    stasl("                                        ");
+                    stasl("               Notepad L                ");
+                    stasl(" ______________________________________ ");
+                    stasl("|              Settings                |");
+                    stasl("|______________________________________|");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                }
+                if (ns == 4)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("               Calculator               ");
+                    stasl("                                        ");
+                    stasl("              File Manager              ");
+                    stasl("                                        ");
+                    stasl("               Notepad L                ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl(" ______________________________________ ");
+                    stasl("|                                      |");
+                    stasl("|______________________________________|");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                }
+                if (ns == 3)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("               Calculator               ");
+                    stasl("                                        ");
+                    stasl("              File Manager              ");
+                    stasl("                                        ");
+                    stasl("               Notepad L                ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl(" ______________________________________ ");
+                    stasl("|                                      |");
+                    stasl("|______________________________________|");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                }
+                if (ns == 2)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("               Calculator               ");
+                    stasl("                                        ");
+                    stasl("              File Manager              ");
+                    stasl("                                        ");
+                    stasl("               Notepad L                ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl(" ______________________________________ ");
+                    stasl("|                                      |");
+                    stasl("|______________________________________|");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                }
+                if (ns == 1)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("               Calculator               ");
+                    stasl("                                        ");
+                    stasl("              File Manager              ");
+                    stasl("                                        ");
+                    stasl("               Notepad L                ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl(" ______________________________________ ");
+                    stasl("|                                      |");
+                    stasl("|______________________________________|");
+                }
+                if (ns == 0)
+                {
+                    stasl("                                        ");
+                    stasl("               SELECT APP               ");
+                    stasl("________________________________________");
+                    stasl("               Calculator               ");
+                    stasl("                                        ");
+                    stasl("              File Manager              ");
+                    stasl("                                        ");
+                    stasl("               Notepad L                ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("                                        ");
+                    stasl("________________________________________");
+                }
+            }
+            Console.WriteLine("|                                                                                                                      |");
+            Console.WriteLine("|                                                                                                                      |");
+            string[] app = ["expl.func", "calculator.func", "notepad.func"];
+            int sapp = 1;
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        sapp++; if (9 == sapp)
+                        {
+                            sapp = 1;
+                        }
+                        Console.Clear(); hapsl(); slct(sapp); break;
+                    case ConsoleKey.DownArrow:
+                        sapp--; if (0 == sapp)
+                        {
+                            sapp = 8;
+                        }
+                        Console.Clear(); hapsl(); slct(sapp); break;
+                    default:
+                        break;
+                }
+            }    
+
+
+                }
+    }
+}
+
